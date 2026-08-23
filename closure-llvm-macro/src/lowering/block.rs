32df581d5351cb13921a28dc1810a2170bcde70f
Chris Foster <fostercm2@gmail.com>
2026-08-23 09:58:20 -0400

    Lower blocks into executable statement IR

--- closure-llvm-macro/src/lowering/block.rs
+++ closure-llvm-macro/src/lowering/block.rs
@@ -1,7 +1,6 @@
 use proc_macro2::TokenStream;
-
+use quote::quote;
 use syn::{ExprAssign, Pat, Stmt, Type};
-
 use crate::lowering::expression::{expression_type, lower_expr, LocalVariable};
 use crate::parser::ClosureArgument;
 
@@ -11,157 +10,138 @@ pub(crate) fn lower_block(
     locals: &[LocalVariable],
     expected_type: Option<&Type>,
 ) -> syn::Result<TokenStream> {
-    lower_stmts(&block.stmts, arguments, locals, expected_type)
+    lower_statements(&block.stmts, arguments, locals, expected_type)
 }
 
-fn lower_stmts(
+fn lower_statements(
     statements: &[Stmt],
     arguments: &[ClosureArgument],
     locals: &[LocalVariable],
     expected_type: Option<&Type>,
 ) -> syn::Result<TokenStream> {
-    let statement = statements.first().ok_or_else(|| {
-        syn::Error::new(proc_macro2::Span::call_site(), "compiled closure blocks cannot be empty")
-    })?;
+    let mut statements_tokens = Vec::new();
+    let mut current_locals = locals.to_vec();
+    let mut result = None;
 
-    match statement {
-        Stmt::Local(local) => {
-            let (name, mutable, explicit_type) = match &local.pat {
-                Pat::Ident(pattern) => (pattern.ident.clone(), pattern.mutability.is_some(), None),
-                Pat::Type(pattern) => {
-                    let explicit_type = (*pattern.ty).clone();
-                    match &*pattern.pat {
-                        Pat::Ident(pattern) => (
-                            pattern.ident.clone(),
-                            pattern.mutability.is_some(),
-                            Some(explicit_type),
-                        ),
-                        _ => return Err(syn::Error::new_spanned(
-                            &pattern.pat,
-                            "let bindings must use identifiers",
-                        )),
+    for (position, statement) in statements.iter().enumerate() {
+        let is_last = position + 1 == statements.len();
+
+        match statement {
+            Stmt::Local(local) => {
+                let (name, mutable, explicit_type) = match &local.pat {
+                    Pat::Ident(pattern) => (pattern.ident.clone(), pattern.mutability.is_some(), None),
+                    Pat::Type(pattern) => {
+                        let explicit_type = (*pattern.ty).clone();
+                        match &*pattern.pat {
+                            Pat::Ident(pattern) => (pattern.ident.clone(), pattern.mutability.is_some(), Some(explicit_type)),
+                            _ => return Err(syn::Error::new_spanned(&pattern.pat, "let bindings must use identifiers")),
+                        }
                     }
-                }
-                _ => return Err(syn::Error::new_spanned(
-                    &local.pat,
-                    "let bindings must use identifiers",
-                )),
-            };
-
-            let initializer = local.init.as_ref().ok_or_else(|| {
-                syn::Error::new_spanned(local, "let bindings require an initializer")
-            })?;
-
-            let local_type = explicit_type.or_else(|| {
-                expression_type(&initializer.expr, arguments, locals)
-            });
-
-            let value = lower_expr(
-                &initializer.expr,
-                arguments,
-                locals,
-                local_type.as_ref(),
-            )?;
-
-            let mut next_locals = locals.to_vec();
-            next_locals.push(LocalVariable {
-                name,
-                value,
-                type_info: local_type,
-                mutable,
-            });
-
-            lower_stmts(&statements[1..], arguments, &next_locals, expected_type)
-        }
-
-        Stmt::Expr(expr, _) => {
-            if let syn::Expr::While(while_expr) = expr {
-                let condition = lower_expr(
-                    &while_expr.cond,
-                    arguments,
-                    locals,
-                    Some(&syn::parse_quote!(bool)),
-                )?;
-
-                let body = lower_block(
-                    &while_expr.body,
-                    arguments,
-                    locals,
-                    None,
-                )?;
-
-                let while_tokens = quote::quote! {
-                    ::closure_llvm::Statement::While {
-                        condition: #condition,
-                        body: ::closure_llvm::Block::expression(#body),
-                    }
+                    _ => return Err(syn::Error::new_spanned(&local.pat, "let bindings must use identifiers")),
                 };
 
-                if statements.len() == 1 {
-                    return Err(syn::Error::new_spanned(
-                        expr,
-                        "while loops require a following result expression",
-                    ));
+                let initializer = local.init.as_ref().ok_or_else(|| {
+                    syn::Error::new_spanned(local, "let bindings require an initializer")
+                })?;
+
+                let local_type = explicit_type.or_else(|| expression_type(&initializer.expr, arguments, &current_locals)).ok_or_else(|| {
+                    syn::Error::new_spanned(&initializer.expr, "cannot infer local variable type")
+                })?;
+
+                let value = lower_expr(&initializer.expr, arguments, &current_locals, Some(&local_type))?;
+                let index = current_locals.len();
+                let type_info = quote! {
+                    <#local_type as ::closure_llvm::CompileType>::type_info()
+                };
+
+                statements_tokens.push(quote! {
+                    ::closure_llvm::Statement::Let {
+                        local: #index,
+                        type_info: #type_info,
+                        value: #value,
+                        mutable: #mutable,
+                    }
+                });
+
+                current_locals.push(LocalVariable {
+                    name,
+                    index,
+                    type_info: Some(local_type),
+                    mutable,
+                });
+            }
+
+            Stmt::Expr(expr, _) => {
+                if let syn::Expr::While(while_expr) = expr {
+                    let condition = lower_expr(
+                        &while_expr.cond,
+                        arguments,
+                        &current_locals,
+                        Some(&syn::parse_quote!(bool)),
+                    )?;
+
+                    let body = lower_block(&while_expr.body, arguments, &current_locals, None)?;
+
+                    statements_tokens.push(quote! {
+                        ::closure_llvm::Statement::While {
+                            condition: #condition,
+                            body: #body,
+                        }
+                    });
+                    continue;
                 }
 
-                let rest = lower_stmts(&statements[1..], arguments, locals, expected_type)?;
-                return Ok(quote::quote! {{ #while_tokens; #rest } });
+                if let syn::Expr::Assign(assign) = expr {
+                    let (index, expected) = assignment_target(assign, &current_locals)?;
+                    let value = lower_expr(&assign.right, arguments, &current_locals, expected)?;
+                    statements_tokens.push(quote! {
+                        ::closure_llvm::Statement::Assign {
+                            local: #index,
+                            value: #value,
+                        }
+                    });
+                    continue;
+                }
+
+                if !is_last {
+                    return Err(syn::Error::new_spanned(expr, "only let bindings, assignments, and while loops may precede the final expression"));
+                }
+
+                result = Some(lower_expr(expr, arguments, &current_locals, expected_type)?);
             }
 
-            if statements.len() == 1 {
-                return lower_expr(expr, arguments, locals, expected_type);
-            }
-
-            if let syn::Expr::Assign(assign) = expr {
-                let next_locals = lower_assignment(assign, arguments, locals)?;
-                return lower_stmts(&statements[1..], arguments, &next_locals, expected_type);
-            }
-
-            Err(syn::Error::new_spanned(
-                &statements[1],
-                "only let bindings, assignments, and while loops may precede the final expression",
-            ))
+            other => return Err(syn::Error::new_spanned(other, "only let bindings, assignments, while loops, and a final expression are supported")),
         }
-
-        other => Err(syn::Error::new_spanned(
-            other,
-            "only let bindings, assignments, while loops, and a final expression are supported",
-        )),
     }
-}
 
-fn lower_assignment(
-    assign: &ExprAssign,
-    arguments: &[ClosureArgument],
-    locals: &[LocalVariable],
-) -> syn::Result<Vec<LocalVariable>> {
-    let name = match &*assign.left {
-        syn::Expr::Path(path) if path.path.segments.len() == 1 => {
-            path.path.segments[0].ident.clone()
-        }
-        _ => return Err(syn::Error::new_spanned(
-            &assign.left,
-            "assignment targets must be local variables",
-        )),
-    };
-
-    let index = locals.iter().rposition(|local| &local.name == &name).ok_or_else(|| {
-        syn::Error::new_spanned(
-            &assign.left,
-            format!("unknown local variable `{}`", name),
-        )
+    let result = result.ok_or_else(|| {
+        syn::Error::new(proc_macro2::Span::call_site(), "closure block must end with an expression")
     })?;
 
-    if !locals[index].mutable {
-        return Err(syn::Error::new_spanned(
-            &assign.left,
-            format!("cannot assign to immutable variable `{}`", name),
-        ));
+    Ok(quote! {
+        ::closure_llvm::Block {
+            statements: vec![#(#statements_tokens),*],
+            result: Some(#result),
+        }
+    })
+}
+
+fn assignment_target<'a>(
+    assign: &ExprAssign,
+    locals: &'a [LocalVariable],
+) -> syn::Result<(usize, Option<&'a Type>)> {
+    let name = match &*assign.left {
+        syn::Expr::Path(path) if path.path.segments.len() == 1 => &path.path.segments[0].ident,
+        _ => return Err(syn::Error::new_spanned(&assign.left, "assignment targets must be local variables")),
+    };
+
+    let local = locals.iter().rev().find(|local| &local.name == name).ok_or_else(|| {
+        syn::Error::new_spanned(&assign.left, format!("unknown local variable `{}`", name))
+    })?;
+
+    if !local.mutable {
+        return Err(syn::Error::new_spanned(&assign.left, format!("cannot assign to immutable variable `{}`", name)));
     }
 
-    let expected_type = locals[index].type_info.as_ref();
-    let value = lower_expr(&assign.right, arguments, locals, expected_type)?;
-    let mut next_locals = locals.to_vec();
-    next_locals[index].value = value;
-    Ok(next_locals)
+    Ok((local.index, local.type_info.as_ref()))
 }
