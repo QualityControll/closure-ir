9971f98bc666be17e174b78ebe330b0d02e9fccd
Chris Foster <fostercm2@gmail.com>
2026-08-23 09:59:05 -0400

    Encode locals as runtime argument slots

--- closure-llvm-macro/src/lowering/path.rs
+++ closure-llvm-macro/src/lowering/path.rs
@@ -13,20 +13,13 @@ pub(crate) fn lower_path(
     if path.path.segments.len() != 1 {
         return Err(syn::Error::new_spanned(path, "only simple identifiers are supported"));
     }
-
     let name = &path.path.segments[0].ident;
-
     if let Some(index) = arguments.iter().position(|argument| &argument.name == name) {
         return Ok(quote! { ::closure_llvm::Expr::Argument(#index) });
     }
-
     if let Some(local) = locals.iter().rev().find(|local| &local.name == name) {
-        let index = local.index;
-        return Ok(quote! { ::closure_llvm::Expr::Local(#index) });
+        let index = arguments.len() + local.index;
+        return Ok(quote! { ::closure_llvm::Expr::Argument(#index) });
     }
-
-    Err(syn::Error::new_spanned(
-        path,
-        format!("unknown closure argument or local variable `{}`", name),
-    ))
+    Err(syn::Error::new_spanned(path, format!("unknown closure argument or local variable `{}`", name)))
 }
