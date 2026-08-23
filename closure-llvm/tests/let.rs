15eb081d57cf1d9c89c66c4119707f6c13309212
Chris Foster <fostercm2@gmail.com>
2026-08-23 08:25:32 -0400

    Add unit tests for let bindings

---
+++ closure-llvm/tests/let.rs
@@ -0,0 +1,82 @@
+use closure_llvm::{call, compile_closure};
+
+
+#[test]
+fn test_let_binding() {
+    let compiled =
+        compile_closure!(|x: i32| -> i32 {
+            let y = x + 1;
+            y * 2
+        });
+
+    assert_eq!(call!(compiled, 4), 10);
+}
+
+
+#[test]
+fn test_multiple_let_bindings() {
+    let compiled =
+        compile_closure!(|x: i32| -> i32 {
+            let y = x + 1;
+            let z = y * 2;
+            z - 3
+        });
+
+    assert_eq!(call!(compiled, 4), 7);
+}
+
+
+#[test]
+fn test_annotated_let_binding() {
+    let compiled =
+        compile_closure!(|x: i32| -> i64 {
+            let y: i64 = x as i64 + 1;
+            y * 2
+        });
+
+    assert_eq!(call!(compiled, 4), 10);
+}
+
+
+#[test]
+fn test_let_binding_in_if_expression() {
+    let compiled =
+        compile_closure!(|x: i32| -> i32 {
+            let y = x + 1;
+
+            if y > 10 {
+                y
+            } else {
+                0
+            }
+        });
+
+    assert_eq!(call!(compiled, 4), 0);
+    assert_eq!(call!(compiled, 10), 11);
+}
+
+
+#[test]
+fn test_let_binding_shadowing() {
+    let compiled =
+        compile_closure!(|x: i32| -> i32 {
+            let y = x + 1;
+            let z = y * 2;
+            let y = x + 10;
+            y + z
+        });
+
+    assert_eq!(call!(compiled, 4), 24);
+}
+
+
+#[test]
+fn test_let_binding_with_tuple_field() {
+    let compiled =
+        compile_closure!(|x: i32| -> i32 {
+            let pair = (x, x + 1);
+            pair.0 + pair.1
+        });
+
+    assert_eq!(call!(compiled, 4), 9);
+}
