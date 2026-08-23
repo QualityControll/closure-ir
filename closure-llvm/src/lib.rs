33233567635eac352a2c06d0d507a381636594ab
Chris Foster <fostercm2@gmail.com>
2026-08-23 09:23:51 -0400

    Export statement IR types

--- closure-llvm/src/lib.rs
+++ closure-llvm/src/lib.rs
@@ -7,7 +7,7 @@ mod types;
 mod value;
 
 pub use compiler::Compiler;
-pub use expr::{Closure, Expr};
+pub use expr::{Block, Closure, Expr, Statement};
 pub use jit::CompiledClosure;
 pub use types::{CompileType, FieldInfo, TypeInfo};
 pub use value::Value;
