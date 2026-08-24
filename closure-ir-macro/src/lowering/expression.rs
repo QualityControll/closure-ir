62c44664ddac1a672bd6e051898e4ff09758f14f
Chris Foster <fostercm2@gmail.com>
2026-08-24 07:56:53 -0400

    Lower intrinsic calls in closure macro

--- closure-ir-macro/src/lowering/expression.rs
+++ closure-ir-macro/src/lowering/expression.rs
@@ -1,7 +1,7 @@
 use proc_macro2::TokenStream;
 use syn::{Expr as SynExpr, Type};
 use crate::parser::ClosureArgument;
-use super::{binary, field, if_else, literal, path, tuple, unary};
+use super::{binary, field, if_else, intrinsic, literal, path, tuple, unary};
 
 #[derive(Clone)]
 pub(crate) struct LocalVariable {
@@ -25,6 +25,7 @@ pub(crate) fn lower_expr(
         SynExpr::If(if_expr) => if_else::lower_if(if_expr, arguments, locals, expected_type),
         SynExpr::Tuple(tuple) => tuple::lower_tuple(tuple, arguments, locals),
         SynExpr::Field(field) => field::lower_field(field, arguments, locals),
+        SynExpr::Call(call) => intrinsic::lower_call(call, arguments, locals, expected_type),
         SynExpr::Paren(paren) => lower_expr(&paren.expr, arguments, locals, expected_type),
         _ => Err(syn::Error::new_spanned(expr, "unsupported expression")),
     }
@@ -46,28 +47,23 @@ pub(crate) fn expression_type(
         SynExpr::Lit(literal) => match &literal.lit {
             syn::Lit::Bool(_) => Some(syn::parse_quote!(bool)),
             syn::Lit::Int(value) => match value.suffix() {
-                "i8" => Some(syn::parse_quote!(i8)),
-                "i16" => Some(syn::parse_quote!(i16)),
-                "i32" | "" => Some(syn::parse_quote!(i32)),
-                "i64" => Some(syn::parse_quote!(i64)),
-                "i128" => Some(syn::parse_quote!(i128)),
-                "u8" => Some(syn::parse_quote!(u8)),
-                "u16" => Some(syn::parse_quote!(u16)),
-                "u32" => Some(syn::parse_quote!(u32)),
-                "u64" => Some(syn::parse_quote!(u64)),
-                "u128" => Some(syn::parse_quote!(u128)),
-                _ => None,
+                "i8" => Some(syn::parse_quote!(i8)), "i16" => Some(syn::parse_quote!(i16)),
+                "i32" | "" => Some(syn::parse_quote!(i32)), "i64" => Some(syn::parse_quote!(i64)),
+                "i128" => Some(syn::parse_quote!(i128)), "u8" => Some(syn::parse_quote!(u8)),
+                "u16" => Some(syn::parse_quote!(u16)), "u32" => Some(syn::parse_quote!(u32)),
+                "u64" => Some(syn::parse_quote!(u64)), "u128" => Some(syn::parse_quote!(u128)), _ => None,
             },
             syn::Lit::Float(value) => match value.suffix() {
-                "f32" => Some(syn::parse_quote!(f32)),
-                "f64" | "" => Some(syn::parse_quote!(f64)),
-                _ => None,
+                "f32" => Some(syn::parse_quote!(f32)), "f64" | "" => Some(syn::parse_quote!(f64)), _ => None,
             },
             _ => None,
         },
         SynExpr::Paren(paren) => expression_type(&paren.expr, arguments, locals),
         SynExpr::Binary(binary) => expression_type(&binary.left, arguments, locals)
             .or_else(|| expression_type(&binary.right, arguments, locals)),
+        SynExpr::Call(call) => {
+            call.args.first().and_then(|arg| expression_type(arg, arguments, locals))
+        }
         _ => None,
     }
 }
