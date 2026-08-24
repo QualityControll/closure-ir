7f58a0fde704888dcdc3d5fd234080fc57293b6a
Chris Foster <fostercm2@gmail.com>
2026-08-23 21:34:33 -0400

    Register intrinsic lowering module

--- closure-ir-macro/src/lowering/mod.rs
+++ closure-ir-macro/src/lowering/mod.rs
@@ -3,10 +3,10 @@ pub(crate) mod block;
 pub(crate) mod expression;
 pub(crate) mod field;
 pub(crate) mod if_else;
+pub(crate) mod intrinsic;
 pub(crate) mod literal;
 pub(crate) mod path;
 pub(crate) mod tuple;
 pub(crate) mod unary;
-
 
 pub(crate) use block::lower_block;
