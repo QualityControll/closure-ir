68d87b421629bcfb6c87152da560af496bda8ab0
Chris Foster <fostercm2@gmail.com>
2026-08-23 21:35:19 -0400

    Lower arithmetic intrinsics into core IR

--- closure-ir-macro/src/lowering/intrinsic.rs
+++ closure-ir-macro/src/lowering/intrinsic.rs
@@ -16,30 +16,42 @@ pub(crate) fn lower_call(
         other => return Err(syn::Error::new_spanned(other, "function calls currently require an intrinsic name")),
     };
     let name = intrinsic_name(function)?;
-    let intrinsic = match name.as_str() {
-        "abs" => "Abs",
-        "min" => "Min",
-        "max" => "Max",
-        "sqrt" => "Sqrt",
-        "pow" => "Pow",
+    let required_args = match name.as_str() {
+        "abs" | "sqrt" => 1,
+        "min" | "max" | "pow" => 2,
         _ => return Err(syn::Error::new_spanned(function, "unsupported intrinsic function")),
-    };
-    let required_args = match intrinsic {
-        "Abs" | "Sqrt" => 1,
-        "Min" | "Max" | "Pow" => 2,
-        _ => unreachable!(),
     };
     if call.args.len() != required_args {
         return Err(syn::Error::new_spanned(call, format!("{} expects {} argument(s)", name, required_args)));
     }
     let lowered = call.args.iter().map(|arg| lower_expr(arg, arguments, locals, expected_type)).collect::<syn::Result<Vec<_>>>()?;
-    let intrinsic = syn::Ident::new(intrinsic, function.path.segments.last().unwrap().ident.span());
-    Ok(quote! {
-        ::closure_ir::Expr::Intrinsic {
-            intrinsic: ::closure_ir::Intrinsic::#intrinsic,
-            arguments: vec![#(#lowered),*],
+
+    match name.as_str() {
+        "abs" => Ok(quote! {
+            ::closure_ir::Expr::IfElse {
+                condition: Box::new(::closure_ir::Expr::Lt {
+                    lhs: Box::new(#lowered[0]),
+                    rhs: Box::new(::closure_ir::Expr::Constant(::closure_ir::Value::I32(0))),
+                }),
+                then_branch: Box::new(::closure_ir::Expr::Neg { operand: Box::new(#lowered[0]) }),
+                else_branch: Box::new(#lowered[0]),
+            }
+        }),
+        "min" | "max" => {
+            let lhs = &lowered[0];
+            let rhs = &lowered[1];
+            let condition = if name == "min" { quote! { ::closure_ir::Expr::Lt { lhs: Box::new(#lhs), rhs: Box::new(#rhs) } } } else { quote! { ::closure_ir::Expr::Gt { lhs: Box::new(#lhs), rhs: Box::new(#rhs) } } };
+            Ok(quote! {
+                ::closure_ir::Expr::IfElse {
+                    condition: Box::new(#condition),
+                    then_branch: Box::new(#lhs),
+                    else_branch: Box::new(#rhs),
+                }
+            })
         }
-    })
+        "sqrt" | "pow" => Err(syn::Error::new_spanned(function, "sqrt and pow are reserved for a future LLVM intrinsic implementation")),
+        _ => unreachable!(),
+    }
 }
 
 fn intrinsic_name(path: &ExprPath) -> syn::Result<String> {
