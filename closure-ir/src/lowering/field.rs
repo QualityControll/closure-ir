10779cca2b03cdea57c5b84615d7750cfdb7d997
Chris Foster <fostercm2@gmail.com>
2026-08-24 08:06:45 -0400

    Fix Lowering impl lifetime

--- closure-ir/src/lowering/field.rs
+++ closure-ir/src/lowering/field.rs
@@ -2,33 +2,14 @@ use inkwell::{builder::Builder, context::Context, values::{FunctionValue, Pointe
 use crate::{expr::Expr, types::TypeInfo};
 use super::{LoweredValue, Lowering};
 
-impl<'ctx> Lowering {
-    pub(crate) fn lower_field(
-        &self,
-        context: &'ctx Context,
-        builder: &Builder<'ctx>,
-        function: FunctionValue<'ctx>,
-        arguments: &[PointerValue<'ctx>],
-        argument_types: &[TypeInfo],
-        expected_type: &TypeInfo,
-        object: &Expr,
-        name: &str,
-    ) -> Result<LoweredValue<'ctx>, String> {
+impl Lowering {
+    pub(crate) fn lower_field(&self, context: &'ctx Context, builder: &Builder<'ctx>, function: FunctionValue<'ctx>, arguments: &[PointerValue<'ctx>], argument_types: &[TypeInfo], expected_type: &TypeInfo, object: &Expr, name: &str) -> Result<LoweredValue<'ctx>, String> {
         let object = self.lower_expr(context, builder, function, arguments, argument_types, expected_type, object)?;
-        let (object_pointer, object_type) = match object {
-            LoweredValue::Pointer { pointer, type_info } => (pointer, type_info),
-            LoweredValue::Value(_) => return Err(format!("cannot access field `{}` on a value", name)),
-        };
-        let fields = match &object_type {
-            TypeInfo::Struct { fields, .. } => fields,
-            _ => return Err(format!("cannot access field `{}` on non-struct type", name)),
-        };
-        let (field_index, field_type) = fields.iter().enumerate()
-            .find_map(|(index, field)| (field.name == name).then(|| (index, field.type_info.clone())))
-            .ok_or_else(|| format!("field `{}` not found", name))?;
+        let (object_pointer, object_type) = match object { LoweredValue::Pointer { pointer, type_info } => (pointer, type_info), LoweredValue::Value(_) => return Err(format!("cannot access field `{}` on a value", name)), };
+        let fields = match &object_type { TypeInfo::Struct { fields, .. } => fields, _ => return Err(format!("cannot access field `{}` on non-struct type", name)), };
+        let (field_index, field_type) = fields.iter().enumerate().find_map(|(index, field)| (field.name == name).then(|| (index, field.type_info.clone()))).ok_or_else(|| format!("field `{}` not found", name))?;
         let struct_type = Self::llvm_struct_type(context, &object_type)?;
-        let field_pointer = builder.build_struct_gep(struct_type, object_pointer, field_index as u32, &format!("{}_ptr", name))
-            .map_err(|error| format!("failed to build GEP for field `{}`: {:?}", name, error))?;
+        let field_pointer = builder.build_struct_gep(struct_type, object_pointer, field_index as u32, &format!("{}_ptr", name)).map_err(|error| format!("failed to build GEP for field `{}`: {:?}", name, error))?;
         Ok(LoweredValue::Pointer { pointer: field_pointer, type_info: field_type })
     }
 }
