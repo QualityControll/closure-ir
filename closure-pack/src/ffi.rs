f6ed717b71b5935d4295585c8d6e7d7a459252df
Chris Foster <fostercm2@gmail.com>
2026-08-25 10:58:55 -0400

    Add external function declarations

---
+++ closure-pack/src/ffi.rs
@@ -0,0 +1,14 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
+pub struct ExternalFunction {
+    pub name: String,
+    pub arguments: Vec<crate::types::TypeInfo>,
+    pub return_type: crate::types::TypeInfo,
+}
+
+impl ExternalFunction {
+    pub fn new<N: Into<String>>(name: N, arguments: Vec<crate::types::TypeInfo>, return_type: crate::types::TypeInfo) -> Self {
+        Self { name: name.into(), arguments, return_type }
+    }
+}
