8009f2146ee2797823848184378593d136cf6b23
Chris Foster <fostercm2@gmail.com>
2026-08-25 08:08:11 -0400

    Use type-erased compiler backend from macro

--- closure-pack-macro/src/compile_closure.rs
+++ closure-pack-macro/src/compile_closure.rs
@@ -15,12 +15,11 @@ fn expand_compile_closure(input: ClosureInput) -> syn::Result<proc_macro2::Token
     let is_unit = matches!(&return_type, syn::Type::Tuple(tuple) if tuple.elems.is_empty());
     let block = if is_unit { lower_block(&body.block, &arguments, &locals, None)? } else { lower_block(&body.block, &arguments, &locals, Some(&return_type))? };
     let argument_type_infos = arguments.iter().map(|argument| { let ty = &argument.type_info; quote! { <#ty as ::closure_pack::CompileType>::type_info() } }).collect::<Vec<_>>();
-    let argument_types = arguments.iter().map(|argument| &argument.type_info).collect::<Vec<_>>();
-    let tuple_type = if argument_types.is_empty() { quote! { () } } else { quote! { (#(#argument_types,)*) } };
     Ok(quote! {{
         let __closure = ::closure_pack::Closure { arguments: vec![#(#argument_type_infos),*], return_type: <#return_type as ::closure_pack::CompileType>::type_info(), body: #block };
         let __context: &'static ::closure_pack::melior::Context = Box::leak(Box::new(::closure_pack::melior::Context::new()));
         let __compiler = ::closure_pack::Compiler::new(__context);
-        __compiler.compile::<#tuple_type, #return_type>(&__closure).expect("failed to compile closure")
+        let (__engine, __function_name) = __compiler.compile_erased(&__closure).expect("failed to compile closure");
+        ::closure_pack::CompiledClosure::from_erased(__engine, __function_name)
     }})
 }
