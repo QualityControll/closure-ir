9e2b62c407246b8f18e260533244899d7f3e22bc
Chris Foster <fostercm2@gmail.com>
2026-08-25 10:35:10 -0400

    Use inferred capture types during closure compilation

--- closure-pack-macro/src/compile_closure.rs
+++ closure-pack-macro/src/compile_closure.rs
@@ -8,7 +8,8 @@ pub(crate) fn expand(input:TokenStream)->TokenStream{let input=parse_macro_input
 
 fn expand_compile_closure(input:ClosureInput)->syn::Result<proc_macro2::TokenStream>{
  let ClosureInput{arguments,return_type,body}=input;
- let captures=captures::discover(&body.block,&arguments);
+ let mut captures=captures::discover(&body.block,&arguments);
+ captures::infer_types(&body.block,&arguments,&mut captures,&return_type);
  let mut lowering_arguments=arguments.clone();
  for capture in &captures {
   let type_info=capture.type_info.clone().ok_or_else(||syn::Error::new(capture.name.span(),format!("cannot infer type of capture `{}`",capture.name)))?;
@@ -22,15 +23,6 @@ fn expand_compile_closure(input:ClosureInput)->syn::Result<proc_macro2::TokenStr
  let tuple_type=if argument_types.is_empty(){quote!{()}}else{quote!{(#(#argument_types,)*)}};
  let capture_names=captures.iter().map(|capture|&capture.name).collect::<Vec<_>>();
  let capture_values=if capture_names.is_empty(){quote!{()}}else{quote!{(#(#capture_names,)*)}};
- let capture_type_infos=captures.iter().map(|capture|{
-  let ty=capture.type_info.as_ref().ok_or_else(||syn::Error::new(capture.name.span(),format!("cannot infer type of capture `{}`",capture.name)))?;
-  Ok(quote!{<#ty as ::closure_pack::CompileType>::type_info()})
- }).collect::<syn::Result<Vec<_>>>()?;
- Ok(quote!{{
-   let __captures= #capture_values;
-   let __closure=::closure_pack::Closure{captures:vec![#(#capture_type_infos),*],arguments:vec![#(#argument_type_infos),*],return_type:<#return_type as ::closure_pack::CompileType>::type_info(),body:#block};
-   let __context:&'static ::closure_pack::melior::Context=Box::leak(Box::new(::closure_pack::melior::Context::new()));
-   let __compiler=::closure_pack::Compiler::new(__context);
-   __compiler.compile_captured::<#tuple_type,#return_type,_>(&__closure,__captures).expect("failed to compile closure")
- }})
+ let capture_type_infos=captures.iter().map(|capture|{let ty=capture.type_info.as_ref().ok_or_else(||syn::Error::new(capture.name.span(),format!("cannot infer type of capture `{}`",capture.name)))?;Ok(quote!{<#ty as ::closure_pack::CompileType>::type_info()})}).collect::<syn::Result<Vec<_>>>()?;
+ Ok(quote!{{let __captures=#capture_values;let __closure=::closure_pack::Closure{captures:vec![#(#capture_type_infos),*],arguments:vec![#(#argument_type_infos),*],return_type:<#return_type as ::closure_pack::CompileType>::type_info(),body:#block};let __context:&'static ::closure_pack::melior::Context=Box::leak(Box::new(::closure_pack::melior::Context::new()));let __compiler=::closure_pack::Compiler::new(__context);__compiler.compile_captured::<#tuple_type,#return_type,_>(&__closure,__captures).expect("failed to compile closure")}})
 }
