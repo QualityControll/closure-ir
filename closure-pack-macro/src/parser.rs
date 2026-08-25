01e656afb1c5a27b05a1fa87178670023adfcb82
Chris Foster <fostercm2@gmail.com>
2026-08-25 10:09:42 -0400

    Fix capture lowering Clone bound

--- closure-pack-macro/src/parser.rs
+++ closure-pack-macro/src/parser.rs
@@ -1,6 +1,7 @@
 use syn::{Expr as SynExpr, ExprBlock, Pat, ReturnType, Type};
 
 pub(crate) struct ClosureInput { pub(crate) arguments: Vec<ClosureArgument>, pub(crate) return_type: Type, pub(crate) body: ExprBlock }
+#[derive(Clone)]
 pub(crate) struct ClosureArgument { pub(crate) name: syn::Ident, pub(crate) type_info: Type, pub(crate) capture: bool }
 impl syn::parse::Parse for ClosureInput {
  fn parse(input:syn::parse::ParseStream<'_>)->syn::Result<Self>{let closure: syn::ExprClosure=input.parse()?;let arguments=closure.inputs.iter().map(|argument|match argument{Pat::Type(pat_type)=>{let name=match &*pat_type.pat{Pat::Ident(pat_ident)=>pat_ident.ident.clone(),_=>return Err(syn::Error::new_spanned(&pat_type.pat,"closure arguments must be identifiers"))};Ok(ClosureArgument{name,type_info:(*pat_type.ty).clone(),capture:false})}_=>Err(syn::Error::new_spanned(argument,"closure arguments must have explicit types"))}).collect::<syn::Result<Vec<_>>>()?;let return_type=match closure.output{ReturnType::Type(_,ty)=>(*ty).clone(),ReturnType::Default=>syn::parse_quote!{()}};let body=match *closure.body{SynExpr::Block(block)=>block,other=>return Err(syn::Error::new_spanned(other,"closure body must be a block"))};Ok(Self{arguments,return_type,body})}
