0ce5364b735ae7174ba1aa3b58eac1502eeaae97
Chris Foster <fostercm2@gmail.com>
2026-08-25 11:02:45 -0400

    Avoid requiring Debug for compiled closure test

--- closure-pack/tests/ffi.rs
+++ closure-pack/tests/ffi.rs
@@ -11,18 +11,11 @@ fn external_function_call_compiles() {
             arguments: vec![Expr::Argument(0)],
             return_type: TypeInfo::F64,
         }),
-        external_functions: vec![ExternalFunction::new(
-            "sqrt",
-            vec![TypeInfo::F64],
-            TypeInfo::F64,
-        )],
+        external_functions: vec![ExternalFunction::new("sqrt", vec![TypeInfo::F64], TypeInfo::F64)],
     };
-
     let context = Box::leak(Box::new(closure_pack::melior::Context::new()));
     let compiler = Compiler::new(context);
-    let _compiled = compiler
-        .compile::<(f64,), f64>(&closure)
-        .expect("external function closure should compile");
+    assert!(compiler.compile::<(f64,), f64>(&closure).is_ok());
 }
 
 #[test]
@@ -38,9 +31,10 @@ fn undeclared_external_function_is_rejected() {
         }),
         external_functions: vec![],
     };
-
     let context = Box::leak(Box::new(closure_pack::melior::Context::new()));
     let compiler = Compiler::new(context);
-    let error = compiler.compile::<(f64,), f64>(&closure).unwrap_err();
-    assert!(error.contains("not_declared"));
+    match compiler.compile::<(f64,), f64>(&closure) {
+        Ok(_) => panic!("undeclared external function should be rejected"),
+        Err(error) => assert!(error.contains("not_declared")),
+    }
 }
